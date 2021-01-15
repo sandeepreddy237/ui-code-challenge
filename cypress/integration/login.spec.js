bea78907cdf09a8e9a4ce9ec29e192da9153f498
Sandeep Reddy <sandeep.reddy@kablamo.com.au>
2021-01-15 13:41:35 +1100

    Update login.spec.js

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -4,12 +4,12 @@ const password = 'Welcome1234!';
 
 context('Login: ', () => {
   describe('Verified', () => {
-    it('Verified user should be successfully complete CTV code verification ', () => {
+    it('Verified user able to login successfully', () => {
       cy.visit(`${Cypress.env('BASE_URL')}`);
       loginPage.loginWith('verified@gmail.com', 'Welcome1234!');
       loginPage.verifyLoginSuccess();
     });
-    it.only('User should able to signup successfully ', () => {
+    it('User should able to signup successfully ', () => {
       cy.visit(`${Cypress.env('BASE_URL')}#screen=signup-email`);
       loginPage.createAccount('random123@gmail.com','Welcome1234!', 'random1234')
     });
